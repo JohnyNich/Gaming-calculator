86797310fb8b8a7c389f63e4d0758c34d344c282
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-01-17 21:58:30 +0800

    Created the input and instancing system
    I've created the block of code which will bring the inputs in (the text docs) and store information on them. Then, it will assign classes to all of them and initiate them with their properties, provided in the text documents.

--- gaming-calculator.py
+++ gaming-calculator.py
@@ -1,4 +1,14 @@
-class Weapon (objec):
+import os
+import linecache
+robocraft_text_docs = []
+robocraft_weapon_class_names = []
+def get_textdoc_line(text_doc_name, line): 
+	print (text_doc_name)
+	print (line)
+	print ("robocraft/" + text_doc_name + ".txt")
+	print (linecache.getline("robocraft/" + text_doc_name + ".txt", line).strip())
+	return linecache.getline("robocraft/" + text_doc_name + ".txt", line).strip()
+class Weapon (object):
 	def __init__(self, name, health, dph, energy, sgfrps): # dpn = Damage per hit, energy = Energy required, sgfrps = Single gun fire rate per second.
 		self.name = name
 		self.health = health
@@ -10,3 +20,23 @@ while True:
 	to_do = to_do.lower()
 	if to_do == "robocraft":
 		to_do_robocraft = input("Do you want to calculate the best or worst weapon?")
+		for info_doc in os.listdir("robocraft"):
+			if info_doc.endswith(".txt") and info_doc != "robocraft-textdoc-structure.txt": # Just in case a file there does not end in .txt.
+				robocraft_text_docs.append(info_doc)
+		for name in robocraft_text_docs: # This is for appending to robocraft_weapon_class_names
+			doc_name = []
+			for letter in name:
+				if letter == ".":
+					break
+				else:
+					doc_name.append(letter)
+			robocraft_weapon_class_names.append("".join(doc_name))
+		print (robocraft_weapon_class_names)
+		for name in robocraft_weapon_class_names:
+			try:
+				exec(name + " = Weapon(" + "\"" + get_textdoc_line(name, 1) + "\"" + ", " + get_textdoc_line(name, 2) + ", " + get_textdoc_line(name, 3) + ", " + get_textdoc_line(name, 4) + ", " + get_textdoc_line(name, 5) + ")")
+				print ("Completed piece of code")
+			except SyntaxError:
+				print ("Failed piece of code")
+				pass
+
